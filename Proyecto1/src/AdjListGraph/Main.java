16150f3038ceeae20c4e9fc12c16db8c73d01b8a
erikaalexandrah <113314381+erikaalexandrah@users.noreply.github.com>
2023-01-25 20:07:31 -0400

    Interface
    Se creó la clase interface.
    Se añadió el modelo de la primera ventana que se ejecutará en App

--- Proyecto1/src/AdjListGraph/Main.java
+++ Proyecto1/src/AdjListGraph/Main.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package AdjListGraph;
-import LinkedList;
+
+import Interface.Interface;
+
 
 /**
  *
@@ -15,13 +17,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        LinkedList myList = new LinkedList(); 
-        
-        myList.append(5);
-        myList.append(4);
-        myList.insertAtHead(3);
-        myList.insertAtPosition("caca", 1);
-        myList.printList();
+        Interface newScreen = new Interface();
+        newScreen.setVisible(true);
+        newScreen.setLocationRelativeTo(null);
         
 
     }
